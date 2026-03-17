e73384d2ac2bc6bacbeef9c20f958d5fccd0a636
Danilo994 <daniloarrabal1@hotmail.com>
2026-03-17 15:43:28 -0300

    analise vendas

--- Linguagem R/Superstore/superstore.R
+++ Linguagem R/Superstore/superstore.R
@@ -37,3 +37,15 @@ ggplot(sales_month, aes(x = Month, y = total_sales, colour = as.factor(Year))) +
 category_sales <- data %>% group_by(Category) %>% summarise(total_sales = sum(Sales), total_profit = sum(Profit))
 ggplot(category_sales, aes(x = Category, y = total_sales)) + geom_bar(stat = "identity")
 ggplot(category_sales, aes(x = Category, y = total_profit)) + geom_bar(stat = "identity")
+
+region_sales <- data %>% group_by(Region) %>% summarise(total_sales = sum(Sales), total_profit = sum(Profit))
+ggplot(region_sales, aes(x = Region, y = total_sales)) + geom_bar(stat = "identity")
+ggplot(region_sales, aes(x = Region, y = total_profit)) + geom_bar(stat = "identity")
+
+ggplot(data, aes(x = Sales, y = Profit)) + geom_point(alpha=0.3)
+
+product_sales <- data %>% group_by(Sub.Category) %>% summarise(total_sales = sum(Sales), total_profit = sum(Profit)) %>% arrange(desc(total_sales))
+ggplot(product_sales, aes(x = reorder(Sub.Category, total_sales), y = total_sales)) + geom_bar(stat="identity") + coord_flip()
+
+data %>% summarise(avg_shipping_time = mean(Ship.Time))
+ggplot(data, aes(x = Ship.Time)) + geom_histogram(binwidth = 1)
